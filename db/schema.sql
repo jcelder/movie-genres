606dfc8cd1b45c7ecbfb88854d1d6366e8b859d8
Joshua Elder <aresx130@gmail.com>
2017-11-02 12:11:14 -0700

    Initial structure of database.

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE movies(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  ratingAverage REAL NOT NULL,
+  releaseDate DATE NOT NULL,
+  genreIDs INT ARRAY
+);
+
+CREATE TABLE genres(
+  id INT PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL,
+  sessionID TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE favorites(
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  movie_id INT NOT NULL
+);
