6e14c124aa652f10cef291c4e5cbd80cbc29c37b
Gulmira Berdalieva <gulmira2410@gmail.com>
2017-11-02 13:43:51 -0700

    add routes and other necessary packages

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 // const app = require('express')()
 const express = require('express')
 const bodyParser = require('body-parser')
+const session = require('cookie-session')
 const routes = require('./routes/index')
 // 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-// 
+
+app.use(session({
+  name: 'session',
+  keys: ['blablabla']
+}))
+
+
 app.use(express.static(__dirname + '/public'))
 // 
 app.set('view engine', 'pug')
 // 
 app.use('/', routes)
 // 
-// app.use((req, res, next) => {
-//   const err = new Error('File not found')
-//   err.status = 404;
-//   next()
-// })
-// 
+app.use((req, res, next) => {
+  const err = new Error('File not found')
+  err.status = 404;
+  next()
+})
+
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: err
+  })
+})
+
 // 
 // 
 // 
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,102 @@ const bodyParser = require('body-parser')
 // 
 // 
 router.get('/', (req, res) => {
-  res.send('Test message please ignore')
+  // res.send('Test message please ignore')
+  return res.render('index', {
+    email: req.session.userID
+  })
 })
-// 
+
+//signup page - GET 
+router.get('/signup', (req, res) => {
+  res.render('signup', {
+    title: 'Sign Up',
+    error: '',
+    email: req.session.userID
+  })
+})
+
+//signup page - POST 
+router.post('/signup', (req, res) => {
+  const {
+    email,
+    password
+  } = req.body 
+  
+  if(!(email || password)) {
+    res.render('signup', {
+      title: 'Sign Up',
+      error: 'Please provide email and password to sign up'
+    })
+  } else {
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      addUser(email, hash)
+        .then(user => {
+          req.session.userID = user.email;
+          res.redirect('/')
+        })
+        .catch(err => {
+          res.render('signup', {
+            title: 'Sign Up',
+            error: 'Could not add user to db'
+          })
+        })
+    })
+  }
+})
+
+//loging page - GET 
+router.get('/login', (req, res) => {
+  res.render('login', {
+    title: 'Log In',
+    error: '',
+    email: req.session.userID
+  })
+})
+
+//login page - POST 
+router.post('/login', (req, res) => {
+  const {
+    email,
+    password 
+  } = req.body 
+  if(!(email || password)) {
+    res.render('login', {
+      title: 'Log In',
+      error: 'Please provide email and password to log in'
+    })
+  } else {
+    getUser(email, password)
+      .then(data => {
+        bcrypt.compare(password, data.password)
+        .then(result => {
+          if(result) {
+            req.session.userID = data.email,
+            res.redirect('/')
+          } else {
+            res.render('login', {
+              title: 'Log In',
+              error: 'Wrong password or email, try again'
+            })
+          }
+        })
+      })
+      .catch(err => {
+        res.render('login', {
+          title: 'Log In',
+          error: 'Could not find this user in db'
+        })
+      })
+  }
+})
+
+
+
+
+
+
+
+
+
 // module.exports = { router }
 module.exports = router;
