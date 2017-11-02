a8e4b2fb0e3d008966669180a11612a37cedf8b8
Joshua Elder <aresx130@gmail.com>
2017-11-02 14:28:53 -0700

    Added return statements to promise functions.

--- db/db_utils.js
+++ db/db_utils.js
@@ -1,13 +1,16 @@
 const { db } = require('./db_connection')
 
 const addUser = (email, hash) => {
-  db.none('INSERT INTO users (email, password)VALUES ($1, $2)', [email, hash])
+  return db.none('INSERT INTO users (email, password)VALUES ($1, $2)', [email, hash])
 }
 
 const getUser = (email) => {
-  db.one('SELECT password FROM users WHERE email=$1 AND pass', [email])
+  return db.one('SELECT password FROM users WHERE email=$1 AND pass', [email])
+}
+
+const getMovies = () => {
+  return db.any('SELECT title FROM movies ORDER BY title')
 }
 
 
-
-module.exports = { addUser, getUser }
+module.exports = { addUser, getUser, }
