6abddd88d03fd3dc5c1f1e56d9604a2fb4c6cb94
Gulmira Berdalieva <gulmira2410@gmail.com>
2017-11-02 13:16:14 -0700

    fix error in routes/index.js

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const app = require('express')()
+// const app = require('express')()
+const express = require('express')
 const bodyParser = require('body-parser')
-
 const routes = require('./routes/index')
+// 
+const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+// 
+app.use(express.static(__dirname + '/public'))
+// 
 app.set('view engine', 'pug')
-
+// 
 app.use('/', routes)
-
-
+// 
+// app.use((req, res, next) => {
+//   const err = new Error('File not found')
+//   err.status = 404;
+//   next()
+// })
+// 
+// 
+// 
+// 
 app.listen(3000, () => {
   console.log('Application running on localhost:3000')
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
-
 const router = express.Router()
-
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+const bodyParser = require('body-parser')
+// 
+// 
 router.get('/', (req, res) => {
   res.send('Test message please ignore')
 })
-
-module.exports = { router }
+// 
+// module.exports = { router }
+module.exports = router;
