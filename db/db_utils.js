3018f5c473b9f20f22bbc0efce971e0cd93be723
Joshua Elder <aresx130@gmail.com>
2017-11-07 14:38:32 -0800

    Added getFavorites().

--- db/db_utils.js
+++ db/db_utils.js
@@ -1,7 +1,9 @@
+
+
 const { db } = require('./db_connection')
 
-const addUser = (email, hash, sessionID) => {
-  return db.one('INSERT INTO users (email, password, sessionID) VALUES ($1, $2, $3) RETURNING users.email', [email, hash, sessionID])
+const addUser = (email, hash) => {
+  return db.one('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING users.email', [email, hash])
 }
 
 const getUser = (email) => {
@@ -12,10 +14,15 @@ const getMovies = () => {
   return db.any('SELECT movies.title FROM movies ORDER BY movies.title')
 }
 
+const getFavorites = (email) => {
+  return db.any(`SELECT movies.title FROM movies JOIN favorites ON movies.id = favorites.movie_id JOIN
+     users ON favorites.user_id = users.id WHERE users.email = $1`, [email])
+}
+
 const addFavorite = async (email, movieTitle) => {
   const userID = await db.one('SELECT users.id FROM users WHERE users.email=$1', [email])
   const movieID = await db.one('SELECT movies.id FROM movies WHERE movies.title=$1', [movieTitle])
   return db.none('INSERT INTO favorites(user_id, movie_id) VALUES($1, $2)', [userID, movieID])
 }
 
-module.exports = { addUser, getUser }
+module.exports = { addUser, getUser, getMovies, getFavorites }
