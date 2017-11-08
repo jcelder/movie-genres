6296189a8b5fb647694036626327cdc071583be8
Joshua Elder <aresx130@gmail.com>
2017-11-07 16:38:51 -0800

    Added seeding of database with default username,password, and favorites for functionality testing.

--- db/seedDB.js
+++ db/seedDB.js
@@ -14,15 +14,24 @@ async function loadData() {
       ($1, $2, $3) RETURNING id`, [movie.title, movie.ratingAverage, movie.releaseDate])
 
     return Promise.all(movie.genreIDs.map((genreID) =>
-        db.none('INSERT INTO movie_genres (movie_id, genre_id) VALUES ($1, $2)', [movieID.id, genreID])
-      )
+      db.none('INSERT INTO movie_genres (movie_id, genre_id) VALUES ($1, $2)', [movieID.id, genreID])
+    ),
     )
   }))
     .catch(err => console.error('movies', err.toString()))
-
-
 }
 
-
+async function seedDB() {
+  // Default Username: me@example.com Password: 1234
+  await db.none(`INSERT INTO users(email, password) VALUES
+  ('me@example.com','$2a$10$9tJUHEuDWkGBcnjwMNLo6.vmPt00CrjcuTY9f7DjiEND9IMRXt7AG')`)
+  await db.none(`INSERT INTO favorites(user_id, movie_id) VALUES
+    ('1','1'),
+    ('1','14'),
+    ('1','18'),
+    ('1','34')`)
+}
 // loadData(() => pgp.end())
-loadData().then(() => pgp.end())
+loadData()
+  .then(() => seedDB())
+  .then(() => pgp.end())
