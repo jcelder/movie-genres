55141a284afe3da7d379ec5607b45e546cadd3e7
Joshua Elder <aresx130@gmail.com>
2017-11-07 16:29:01 -0800

    Added getDetailedMovieInfo(), movieFavoriteCheck(), deleteFavorite().

--- db/db_utils.js
+++ db/db_utils.js
@@ -11,7 +11,10 @@ const getUser = (email) => {
 }
 
 const getDetailedMovieInfo = (title) => {
-  return db.one('SELECT movies.title, movies.ratingAverage, FROM movies WHERE movies.title=$1', [title])
+  return db.one(`SELECT movies.title, movies.ratingAverage, movies.releaseDate, string_agg(genres.name, ', ') as Genres
+      FROM movies JOIN movie_genres ON movies.id = movie_genres.movie_id
+      JOIN genres ON movie_genres.genre_id = genres.id WHERE movies.title=$1
+      GROUP BY movies.title, movies.ratingAverage, movies.releaseDate`, [title])
 }
 
 const getMovies = () => {
@@ -23,10 +26,31 @@ const getFavorites = (email) => {
      users ON favorites.user_id = users.id WHERE users.email = $1`, [email])
 }
 
-const addFavorite = async (email, movieTitle) => {
+const movieFavoriteCheck = async (email, movieTitle, releaseDate) => {
   const userID = await db.one('SELECT users.id FROM users WHERE users.email=$1', [email])
-  const movieID = await db.one('SELECT movies.id FROM movies WHERE movies.title=$1', [movieTitle])
+  const movieID = await db.one('SELECT movies.id FROM movies WHERE movies.title=$1 AND movies.releaseDate=$2', [movieTitle, releaseDate])
+  return db.oneOrNone('SELECT id FROM favorites WHERE user_id=$1 AND movie_id=$2', [userID, movieID])
+}
+
+const addFavorite = async (email, movieTitle, releaseDate) => {
+  const userID = await db.one('SELECT users.id FROM users WHERE users.email=$1', [email])
+  const movieID = await db.one('SELECT movies.id FROM movies WHERE movies.title=$1 AND movies.releaseDate=$2', [movieTitle, releaseDate])
   return db.none('INSERT INTO favorites(user_id, movie_id) VALUES($1, $2)', [userID, movieID])
 }
 
-module.exports = { addUser, getUser, getMovies, getFavorites }
+const deleteFavorite = async (email, movieTitle, releaseDate) => {
+  const userID = await db.one('SELECT users.id FROM users WHERE users.email=$1', [email])
+  const movieID = await db.one('SELECT movies.id FROM movies WHERE movies.title=$1 AND movies.releaseDate=$2', [movieTitle, releaseDate])
+  return db.none('DELETE FROM favorites WHERE user_id=$1 AND movie_id=$2', [userID, movieID])
+}
+
+module.exports = {
+  addUser,
+  getUser,
+  getMovies,
+  getFavorites,
+  getDetailedMovieInfo,
+  addFavorite,
+  movieFavoriteCheck,
+  deleteFavorite,
+}
